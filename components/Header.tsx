295b1a4efc81e440c5f308cae34b673c3a581f07
Ankitkumar <ap774937@gmail.com>
2026-08-03 12:34:37 +0530

    Testing Poll SCM

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
 
         <div>
           <h1 className="text-xl font-bold text-gray-800">
-            Lexavra AI Assistant
+            Lexavra AI Assistant ChatBot
           </h1>
 
           <p className="text-sm text-gray-500">
